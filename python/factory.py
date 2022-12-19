b9dc2d331b61f144c3df36c81defa42f16259a78
Matthew Carrothers <matthew.carrothers@bjss.com>
2022-12-19 16:07:42 +0000

    Removed redundant code.

--- python/factory.py
+++ python/factory.py
@@ -5,16 +5,13 @@ class Factory:
     def build_item(self, entry):
         if entry[0] == "Aged Brie":
             context = Context(BrieStrategy())
-            item = context.build_item(entry)
         elif entry[0] == "Backstage passes to a TAFKAL80ETC concert":
             context = Context(BackstageStrategy())
-            item = context.build_item(entry)
         elif entry[0] == "Sulfuras, Hand of Ragnaros":
             context = Context(LegendaryStrategy())
-            item = context.build_item(entry)
         else:
             context = Context(BasicStrategy())
-            item = context.build_item(entry)
+        item = context.build_item(entry)
         return item
 
         # if 'else' in build_item:
