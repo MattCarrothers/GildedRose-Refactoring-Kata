3b883479014db66cc71ff748a7f2e2b86933a1e0
Matthew Carrothers <matthew.carrothers@bjss.com>
2022-12-19 16:38:19 +0000

    Moved towards Strategy Design pattern.

--- python/context_class.py
+++ python/context_class.py
@@ -33,14 +33,6 @@ class Context:
 
 class Strategy(ABC):
 
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
-
     def adjust_quality(self):
         pass
 
