69ec984eeca73e2e61f84544f9e31db8c1160301
Matthew Carrothers <matthew.carrothers@bjss.com>
2022-12-20 10:34:32 +0000

    Strategy design. (Currently Broken.)

--- python/factory.py
+++ python/factory.py
@@ -4,13 +4,9 @@ from context_class import *
 class Factory:
     def build_item(self, entry):
         if entry[0] == "Aged Brie":
-            context = Context(BrieStrategy())
         elif entry[0] == "Backstage passes to a TAFKAL80ETC concert":
-            context = Context(BackstageStrategy())
         elif entry[0] == "Sulfuras, Hand of Ragnaros":
-            context = Context(LegendaryStrategy())
         else:
-            context = Context(BasicStrategy())
         return context
 
         # if 'else' in build_item:
